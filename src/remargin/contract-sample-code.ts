d2c07582f5c0235132549099737e7bcc1b77f134
ChampFu <champfu@mcdex.io>
2021-10-18 11:30:17 +0800

    update remargin sample

--- src/remargin/contract-sample-code.ts
+++ src/remargin/contract-sample-code.ts
@@ -9,23 +9,28 @@ import {
 
 async function setupPosition(liquidityPool: any, trader: any) {
   // setTargetLeverage if you didn't set. 5x Leverage.
-  await liquidityPool.connect(trader).setTargetLeverage(0, trader.address, toWei("5"))
+  let marginAccount = await liquidityPool.getMarginAccount(0, trader.address)
+  let targetLeverage = fromWei(marginAccount.targetLeverage.toString())
+  if (targetLeverage != 5) {
+    await liquidityPool.connect(trader).setTargetLeverage(0, trader.address, toWei("5"))
+    console.log("Set leverage to 5");
+  }
 
   // execute trade(): open position
-  await liquidityPool.connect(trader).trade(0, trader.address, toWei("1"), toWei("3600"), Math.floor(Date.now()/1000)+999999, NONE, USE_TARGET_LEVERAGE)
+  await liquidityPool.connect(trader).trade(0, trader.address, toWei("1"), toWei("4000"), Math.floor(Date.now()/1000)+999999, NONE, USE_TARGET_LEVERAGE)
 }
 
-async function remargin(liquidityPool: any, trader: any) {
+async function remarginFrom5To2(liquidityPool: any, trader: any) {
+  // If we want to set leverage from 5 to 2, 30 USDC is Keeper Gas Reward.
   // we already opened a position 1 with 5x leverage.
   let marginAccount = await liquidityPool.getMarginAccount(0, trader.address)
   let position = fromWei(marginAccount.position.toString())
   let margin = fromWei(marginAccount.margin.toString())
   let targetLeverage = fromWei(marginAccount.targetLeverage.toString())
-  console.log("should be 1", position)
-  console.log("should be 5", targetLeverage)
-
-  // If we want to set leverage from 5 to 2, 30 USDC is Keeper Gas Reward.
+  console.log("should be 1 = ", position)
+  console.log("should be 5 = ", targetLeverage)
   console.log("Margin", margin)
+
   let depositAmount = (margin - 30) * 5 / 2 - margin
   console.log("depositAmount", depositAmount)
 
@@ -34,9 +39,37 @@ async function remargin(liquidityPool: any, trader: any) {
   position = fromWei(marginAccount.position.toString())
   margin = fromWei(marginAccount.margin.toString())
   targetLeverage = fromWei(marginAccount.targetLeverage.toString())
-  console.log("should be 1", position)
-  console.log("should be 5", targetLeverage)
+  console.log("should be 1 = ", position)
+  console.log("should be 5 = ", targetLeverage)
   console.log("Margin", margin)
+
+  // then we can set leverage to 2
+  await liquidityPool.connect(trader).setTargetLeverage(0, trader.address, toWei("2"))
+}
+
+async function remarginFrom2To10(liquidityPool: any, trader: any) {
+  // If we want to set leverage from 5 to 2, 30 USDC is Keeper Gas Reward.
+  let marginAccount = await liquidityPool.getMarginAccount(0, trader.address)
+  let position = fromWei(marginAccount.position.toString())
+  let margin = fromWei(marginAccount.margin.toString())
+  let targetLeverage = fromWei(marginAccount.targetLeverage.toString())
+  console.log("should be 1 = ", position)
+  console.log("should be 2 = ", targetLeverage)
+  console.log("Margin", margin)
+
+  let depositAmount = margin - (margin - 30) * 2 / 10
+  await liquidityPool.connect(trader).withdraw(0, trader.address, toWei(depositAmount.toString()))
+
+  marginAccount = await liquidityPool.getMarginAccount(0, trader.address)
+  position = fromWei(marginAccount.position.toString())
+  margin = fromWei(marginAccount.margin.toString())
+  targetLeverage = fromWei(marginAccount.targetLeverage.toString())
+  console.log("should be 1 = ", position)
+  console.log("should be 2 = ", targetLeverage)
+  console.log("Margin", margin)
+
+  // then we can set leverage to 10
+  await liquidityPool.connect(trader).setTargetLeverage(0, trader.address, toWei("10"))
 }
 
 async function main() {
@@ -48,7 +81,8 @@ async function main() {
   const liquidityPool = new ethers.Contract(liquidityPoolAddress, liquidityPoolABI.abi, ethers.provider)
 
   await setupPosition(liquidityPool, trader)
-  await remargin(liquidityPool, trader)
+  await remarginFrom5To2(liquidityPool, trader)
+  await remarginFrom2To10(liquidityPool, trader)
 }
 
 main()
