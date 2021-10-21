75bdaf9c621cdf68be53898c100c6bbc9997587e
ChampFu <champfu@mcdex.io>
2021-10-21 18:30:40 +0800

    update funding from reader

--- sdk-sample-code/funding.ts
+++ sdk-sample-code/funding.ts
@@ -1,6 +1,6 @@
 import {ensureFinished, fromWei, NONE, toWei, USE_TARGET_LEVERAGE} from "../utils/utils";
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { LiquidityPoolFactory } from '@mcdex/mai3.js';
+import { LiquidityPoolFactory, getLiquidityPool, getReaderContract } from '@mcdex/mai3.js';
 import * as dotenv from "dotenv";
 dotenv.config({ path: '~/.env' });
 
@@ -9,14 +9,18 @@ async function main() {
   const liquidityPoolAddress = "0xc32a2dfee97e2babc90a2b5e6aef41e789ef2e13"
   const provider = new JsonRpcProvider('https://rinkeby.arbitrum.io/rpc')
   // @ts-ignore
-  const liquidityPool = LiquidityPoolFactory.connect(liquidityPoolAddress, provider)
+  const reader = await getReaderContract(provider)
+  const pool = await getLiquidityPool(reader, liquidityPoolAddress)
 
-  // Take perpetualIndex 0 for example: index 3 of perpetualInfo is fundingRate
-  const {nums} = await liquidityPool.getPerpetualInfo(0)
-  console.log("fundingRate " + fromWei(nums[3].toString()))
-
-  let unitAccumulativeFunding = fromWei(nums[4].toString())
-  console.log("unitAccumulativeFunding " + unitAccumulativeFunding)
+  // Take perpetualIndex 0 for example:
+  const perpetual = pool.perpetuals.get(0)
+  if (perpetual == undefined) {
+    console.log("perpetual is undefined")
+    return
+  }
+  console.log("fundingRate: " + perpetual.fundingRate.toString())
+  const unitAccumulativeFunding = perpetual.unitAccumulativeFunding.toString()
+  console.log("unitAccumulativeFunding: " + unitAccumulativeFunding)
   console.log("Funding payment = entryFunding - position(1) * unitAccumulativeFunding (" + unitAccumulativeFunding + "), entryFunding from MarginAccount of MAI3-graph")
 }
 
