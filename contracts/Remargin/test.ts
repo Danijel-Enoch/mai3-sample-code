48dbf6b4b8868c7129b96ff9b23ca3363367fa75
ChampFu <champfu@mcdex.io>
2021-10-22 12:17:00 +0800

    update remargin

--- contracts/Remargin/test.ts
+++ contracts/Remargin/test.ts
@@ -1,5 +1,6 @@
 // @ts-ignore
 import {ethers} from "hardhat";
+import {fromWei, toWei} from "../../utils/utils";
 
 async function test() {
   const Contract = await ethers.getContractFactory("Remargin");
@@ -7,6 +8,15 @@ async function test() {
 
   await contract.deployed();
   console.log("Greeter deployed to:", contract.address);
+  const {keeperGasReward, markPrice, margin, position, targetMargin, amount} = await contract.callStatic.calAmountWithLeverage(
+    "0xc32a2dfee97e2babc90a2b5e6aef41e789ef2e13", 0, toWei("3")
+  )
+  console.log("keeperGasReward: " + fromWei(keeperGasReward.toString()))
+  console.log("markPrice: " + fromWei(markPrice.toString()))
+  console.log("position: " + fromWei(position.toString()))
+  console.log("margin: " + fromWei(margin.toString()))
+  console.log("targetMargin: " + fromWei(targetMargin.toString()))
+  console.log("amount: " + fromWei(amount.toString()))
 }
 
 test().catch((error) => {
