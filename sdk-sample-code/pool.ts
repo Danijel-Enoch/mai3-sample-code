4b8692d0a6a79d364898dd32ce11ea08420049fe
ChampFu <champfu@mcdex.io>
2021-11-03 10:10:10 +0800

    add pool

---
+++ sdk-sample-code/pool.ts
@@ -0,0 +1,21 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { getLiquidityPool, getReaderContract } from '@mcdex/mai3.js';
+import * as dotenv from "dotenv";
+dotenv.config({ path: '~/.env' });
+
+async function main() {
+  const liquidityPoolAddress = "0xc32a2dfee97e2babc90a2b5e6aef41e789ef2e13"
+  const provider = new JsonRpcProvider('https://rinkeby.arbitrum.io/rpc')
+
+  // @ts-ignore
+  const reader = await getReaderContract(provider)
+  const pool = await getLiquidityPool(reader, liquidityPoolAddress)
+  console.log("pool ", pool)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
